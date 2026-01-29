15a49b9ddff6726a9f8f7da785586e24e04e9162
RidwanHR5 <ridwanhusain106@gmail.com>
2026-01-29 13:34:31 +0600

    cleaned up unnecessary imports

--- colpali_engine/trainer/__init__.py
+++ colpali_engine/trainer/__init__.py
@@ -1,3 +1,2 @@
-from .colmodel_torch_training import ColModelTorchTraining
 from .colmodel_training import ColModelTraining, ColModelTrainingConfig
 from .contrastive_trainer import ContrastiveTrainer
